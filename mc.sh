e138bf1ea4f64c8147137ff3fbb25ab34e4b8181
Henrique Varela <79428566+henrique11varela@users.noreply.github.com>
2026-01-05 07:00:13 -0800

    Add script to install Cloudflared and configure access
    Install Cloudflared if not already installed and set up access.

---
+++ mc.sh
@@ -0,0 +1,10 @@
+if [ ! -f ~/cloudflared_installed ]; then
+	sudo apt update && sudo apt upgrade -y && sudo apt install curl vim -y
+	sudo mkdir -p --mode=0755 /usr/share/keyrings
+	curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+	echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+	sudo apt update && sudo apt install cloudflared
+	touch ~/cloudflared_installed
+fi
+echo "to connect use localhost:9210"
+cloudflared access tcp --hostname minecraft2.henriquevarela.app --url 127.0.0.1:9210
